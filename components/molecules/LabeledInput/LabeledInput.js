fab3060e21f0c507661f60312b0d5f07688ff879
nickm980 <81270095+nickm980@users.noreply.github.com>
2021-06-07 20:51:44 -0400

    feature: Post preview

--- components/molecules/LabeledInput/LabeledInput.js
+++
@@ -1,9 +0,0 @@
-import styles from "components/molecules/LabeledInput/LabeledInput.css";
-
-export default function LabeledInput(props) {
-  return (
-    <form>
-      <label></label>
-    </form>
-  );
-}

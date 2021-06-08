92dc8553d89c316a1f5a5bb33d62ae2ba997e955
nickm980 <81270095+nickm980@users.noreply.github.com>
2021-06-07 22:00:17 -0400

    feature: Post preview

---
+++ components/PostPreview/PostPreview.tsx
@@ -0,0 +1,14 @@
+import styles from "components/PostPreview/PostPreview.module.css";
+import { Post, User } from "@prisma/client";
+
+export default function PostPreview(props) {
+  const user: User = props.user;
+  const post: Post = props.post;
+
+  return (
+    <article>
+      <h2>{post.title}</h2>
+      <p>{post.description}</p>
+    </article>
+  );
+}
